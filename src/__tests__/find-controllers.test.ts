b3bf4dc4a8f4768e6865b4c7865b3fa000cfa119
Jeremy Clarke <jeremy@savetrees.co.uk>
2024-11-19 16:37:47 +0000

    feat(findControllers): add support for ESM

--- src/__tests__/find-controllers.test.ts
+++ src/__tests__/find-controllers.test.ts
@@ -1,8 +1,11 @@
-import { findControllers } from '../find-controllers'
+import { FindControllersResult, findControllers } from '../find-controllers'
 
 describe('findControllers', () => {
-  it('returns the classes from the fixtures', () => {
-    const result = findControllers('__fixtures__/*.js', { absolute: true })
+  it('(sync) returns the classes from the fixtures', () => {
+    const result = findControllers('__fixtures__/*.js', {
+      absolute: true,
+    })
+
     const moduleExports = result.find((x: any) => x.target.isModuleExports)
     const defaultExports = result.find((x: any) => x.target.isDefaultExport)
     const namedExports = result.find((x: any) => x.target.isNamedExport)
@@ -30,5 +33,40 @@ describe('findControllers', () => {
     expect(typeof (mixedModuleExports as any).target).toBe('function')
     expect(mixedModuleDefaultExports).toBeUndefined()
     expect(mixedModuleNamedExports).toBeUndefined()
+  })
+
+  it('(async) returns the classes from the fixtures', async () => {
+    const result = await findControllers('__fixtures__/*.js', {
+      absolute: true,
+      esModules: true,
+    })
+
+    const moduleExports = result.find((x: any) => x.target.isModuleExports)
+    const defaultExports = result.find((x: any) => x.target.isDefaultExport)
+    const namedExports = result.find((x: any) => x.target.isNamedExport)
+    const mixedDefaultExports = result.find(
+      (x: any) => x.target.isMixedDefaultExport,
+    )
+    const mixedNamedExports = result.find(
+      (x: any) => x.target.isMixedNamedExport,
+    )
+    const mixedModuleExports = result.find(
+      (x: any) => x.target.isMixedModuleExports,
+    )
+    const mixedModuleDefaultExports = result.find(
+      (x: any) => x.target.isMixedModuleDefaultExport,
+    )
+    const mixedModuleNamedExports = result.find(
+      (x: any) => x.target.isMixedModuleNamedExport,
+    )
+    expect(result.length).toBe(5)
+    expect(typeof (moduleExports as any).target).toBe('function')
+    expect(typeof (namedExports as any).target).toBe('function')
+    expect(typeof (mixedNamedExports as any).target).toBe('function')
+    expect(typeof (mixedModuleExports as any).target).toBe('function')
+    expect(typeof (mixedModuleNamedExports as any).target).toBe('function')
+    expect(defaultExports).toBeUndefined()
+    expect(mixedModuleDefaultExports).toBeUndefined()
+    expect(mixedDefaultExports).toBeUndefined()
   })
 })
--- src/find-controllers.ts
+++ src/find-controllers.ts
@@ -1,44 +1,69 @@
 import * as glob from 'fast-glob'
+
 import { IStateAndTarget, getStateAndTarget } from './state-util'
+
+/**
+ * Find Controllers options for module finding and import resolution.
+ */
+export type FindControllersOptions<ESM extends boolean = false> =
+  glob.Options & { esModules?: ESM }
 
 /**
  * Find Controllers result.
  */
 export type FindControllersResult = Array<IStateAndTarget>
 
+export function findControllers<ESM extends boolean = false>(
+  pattern: string,
+  opts?: FindControllersOptions<ESM>,
+): ESM extends true ? Promise<FindControllersResult> : FindControllersResult
 /**
  * Finds classes using the specified pattern and options.
  *
  * @param pattern Glob pattern
- * @param opts Glob options
+ * @param opts Glob options and ES modules dynamic import module loading
  */
-export function findControllers(
+export function findControllers<ESM extends boolean>(
   pattern: string,
-  opts?: glob.Options,
-): FindControllersResult {
+  opts?: FindControllersOptions<ESM>,
+): Promise<FindControllersResult> | FindControllersResult {
   const result = glob.sync(pattern, opts)
-  return result
-    .map((path) => {
-      const items: Array<IStateAndTarget | null> = []
 
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const required = require(path)
+  if (opts && opts.esModules === true) {
+    return Promise.all(
+      result.map((path) => import(path).then(extractStateAndTargetFromExports)),
+    ).then((controllers) =>
+      controllers.reduce((acc, cur) => acc.concat(cur), []),
+    )
+  } else {
+    return result
+      .map((path) => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const exports = require(path)
+        return extractStateAndTargetFromExports(exports)
+      })
+      .reduce((acc, cur) => acc.concat(cur), [])
+  }
+}
 
-      if (required) {
-        const stateAndTarget = getStateAndTarget(required)
-        if (stateAndTarget) {
-          items.push(stateAndTarget)
-          return items
-        }
+function extractStateAndTargetFromExports(exports: any): FindControllersResult {
+  const items: FindControllersResult = []
 
-        // loop through exports - this will cover named as well as a default export
-        for (const key of Object.keys(required)) {
-          items.push(getStateAndTarget(required[key]))
-        }
-      }
-
+  if (exports) {
+    const stateAndTarget = getStateAndTarget(exports)
+    if (stateAndTarget) {
+      items.push(stateAndTarget)
       return items
-    })
-    .reduce((acc, cur) => acc.concat(cur), [])
-    .filter((x) => x !== null) as FindControllersResult
+    }
+
+    // loop through exports - this will cover named as well as a default export
+    for (const key of Object.keys(exports)) {
+      const stateAndTarget = getStateAndTarget(exports[key])
+      if (stateAndTarget) {
+        items.push(stateAndTarget)
+      }
+    }
+  }
+
+  return items
 }
